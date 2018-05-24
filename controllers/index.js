9a459ab9e198f9462a6d1972124a2fd7a679917c
Lino Silva <lino.silva@farfetch.com>
2018-05-24 21:18:09 +0100

    Updated map, added favicon

--- controllers/index.js
+++ controllers/index.js
@@ -8,7 +8,7 @@ const index = function index(req, res) {
     },
     map: {
       embed:
-        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2987.005057605059!2d-8.62069658498627!3d41.5258310953648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24522d76c1d1b3%3A0xc4920dc3bffa3933!2sCasa+do+Egipto!5e0!3m2!1sen!2spt!4v1508794803221',
+        'https://www.google.com/maps/embed?key=AIzaSyB2Rb0yTldj51ckj26NNehNvk8uJ49Ho7s&pb=!1m18!1m12!1m3!1d2704.4898780667054!2d-8.622057502461676!3d41.526257550967614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24522d76c1d1b3%3A0xc4920dc3bffa3933!2sCasa+do+Egipto!5e0!3m2!1spt-PT!2spt!4v1527192683868',
     },
     footer: {
       rsvpDate: '1 de Novembro',
