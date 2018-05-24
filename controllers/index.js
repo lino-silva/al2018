bcfc91d283483545f58c224fa003c8090fd096c2
Lino Silva <lino.silva@farfetch.com>
2018-05-24 22:41:53 +0100

    Changed fonts and background image

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,6 @@
 const index = function index(req, res) {
   res.render('home', {
     title: 'Ana & Lino',
-    header: {
-      title: '"A festa"',
-      subtitle: '01.12.2018',
-      names: 'Ana e Lino',
-    },
     map: {
       embed:
         'https://www.google.com/maps/embed?key=AIzaSyB2Rb0yTldj51ckj26NNehNvk8uJ49Ho7s&pb=!1m18!1m12!1m3!1d2704.4898780667054!2d-8.622057502461676!3d41.526257550967614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24522d76c1d1b3%3A0xc4920dc3bffa3933!2sCasa+do+Egipto!5e0!3m2!1spt-PT!2spt!4v1527192683868',
